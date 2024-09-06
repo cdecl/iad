3809db38e37b679ff519dc3d5dad7999fd6157c9
Byung Kyu KIM <cdeclare@gmail.com>
2024-09-06 23:26:17 +0900

    feat: telegram bot init

--- bot.py
+++ bot.py
@@ -15,8 +15,8 @@ logging.basicConfig(
 
 # /start 및 /help 명령어 핸들러
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    command_list = ["/start", "/place", "/home", "/fav"]
-    response = "사용 가능한 명령어 목록:\n" + "\n".join(command_list)
+    command_list = ["/start /s", "/place /p", "/home /h", "/fav /f"]
+    response = "사용 가능한 명령어:\n" + "\n".join(command_list)
     await update.message.reply_text(response)
 
 
@@ -27,42 +27,42 @@ async def handle_place(update: Update, context: ContextTypes.DEFAULT_TYPE):
             order_in = context.args[0]
 
             if not order_in.isnumeric:
-                raise Exception("순서값 에러")
+                raise Exception("질문오류 → ex> 1 장소명")
             order = int(context.args[0])
             q = ' '.join(context.args[1:])
 
             r = place(order, "100", q)
             await update.message.reply_text(f'{r}')
         else:
-            raise Exception("질문 형식 오류")
+            raise Exception("질문오류 → ex> 1 장소명")
     except Exception as e:
-        await update.message.reply_text(f"except : {e}")
+        await update.message.reply_text(f"except: {e}")
 
 
 async def handle_home(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         q = ' '.join(context.args)
         if not q:
-            raise Exception('질문 오류')
+            raise Exception('질문오류 → /h 장소이름')
 
         r = home(q)
         await update.message.reply_text(f'{r}')
 
     except Exception as e:
-        await update.message.reply_text(f"except : {e}")
+        await update.message.reply_text(f"except: {e}")
 
 
 async def handle_fav(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         q = ' '.join(context.args)
         if not q:
-            raise Exception('질문 오류')
+            raise Exception('질문오류 → /f 장소이름')
 
         r = fav(q)
         await update.message.reply_text(f'{r}')
 
     except Exception as e:
-        await update.message.reply_text(f"except : {e}")
+        await update.message.reply_text(f"except: {e}")
 
 
 # 알 수 없는 명령어 처리
@@ -76,12 +76,12 @@ def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler(["start", "help"], start))
-    app.add_handler(CommandHandler("place", handle_place))
-    app.add_handler(CommandHandler("home", handle_home))
-    app.add_handler(CommandHandler("fav", handle_fav))
+    app.add_handler(CommandHandler(["place", "p"], handle_place))
+    app.add_handler(CommandHandler(["home", "h"], handle_home))
+    app.add_handler(CommandHandler(["fav", "f"], handle_fav))
     app.add_handler(MessageHandler(filters.COMMAND, unknown))
 
-    print('봇이 실행되었습니다...')
+    print('Bot Start ...')
     app.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
