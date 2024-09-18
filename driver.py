5934df2a8139d9233c7bca1b983838f53bb04594
Byung Kyu KIM <cdeclare@gmail.com>
2024-09-18 21:44:40 +0900

    fix: create_common_driver debugMode -> headless 인터페이스 변경

--- driver.py
+++ driver.py
@@ -8,14 +8,15 @@ import unittest
 import json
 
 
-def create_common_driver(user_agent: str, debugMode: bool = False):
+def create_common_driver(user_agent: str, headless: bool):
     USER_AGENT = user_agent
     chrome_options = Options()
 
-    if debugMode:
-        chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+    # if debugMode:
+    #     chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+    if headless:
+        chrome_options.add_argument("--headless=new")  # 헤드리스 모드
 
-    chrome_options.add_argument("--headless=new")  # 헤드리스 모드
     chrome_options.add_argument("--disable-gpu")  # GPU 비활성화
     chrome_options.add_argument("--no-sandbox")  # 샌드박스 비활성화
     chrome_options.add_argument("--disable-dev-shm-usage")  # /dev/shm 사용 비활성화
@@ -24,23 +25,25 @@ def create_common_driver(user_agent: str, debugMode: bool = False):
     chrome_options.add_argument(f"user-agent={USER_AGENT}")
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-    return driver   
+    return driver
 
-def create_driver(debugMode: bool = False):
+
+def create_driver(headless: bool = True):
     user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
-    return create_common_driver(user_agent, debugMode)
+    return create_common_driver(user_agent, headless)
 
-def create_mobile_driver(debugMode: bool = False):
+
+def create_mobile_driver(headless: bool = True):
     user_agent = r'Mozilla/5.0 (iPhone; CPU iPhone OS 18_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0 Mobile/15E148 Safari/604.1'
-    return create_common_driver(user_agent, debugMode)
+    return create_common_driver(user_agent, headless)
 
 
 class TestCase01(unittest.TestCase):
-    def test_http(self): 
+    def test_http(self):
         url = 'https://httpbin.org/get'
         driver = create_driver()
         driver.get(url)
-        
+
         body = driver.find_element(By.CSS_SELECTOR, 'body')
         dicBody= json.loads(body.text)
 
